8f66f5777c0c4775159093db23114f0039737995
edow <ethandow@gmail.com>
2018-04-02 13:20:40 -0400

    working on 4a

--- ps4a.py
+++ ps4a.py
@@ -3,8 +3,9 @@
 # Collaborators:
 # Time Spent: x:xx
 
+
 def get_permutations(sequence):
-    '''
+    """
     Enumerate all permutations of a given string
 
     sequence (string): an arbitrary string to permute. Assume that it is a
@@ -21,20 +22,58 @@ def get_permutations(sequence):
 
     Note: depending on your implementation, you may return the permutations in
     a different order than what is listed here.
-    '''
+    """
 
-    pass #delete this line and replace with your code here
+
+
+    # method that inserts first character into permutation of remaining characters
+    def permutations_into_an_item(letter, item):
+
+
+
+    # base case
+    sequence = [sequence]
+    if len(sequence[0]) == 1:
+        return sequence
+    # recursive case
+    # A
+    # AB, BA
+    # CAB, ACB, ABC... CBA, BCA, BAC
+    else:
+        for letter in se:n= sequence[0]
+        sequence_for_permutations = sequence[1:len(sequence)]
+        print("Sequence for permutations: {}, Letter: {}".format(sequence_for_permutations, letter))
+        permutation_list = []
+        for item in range(0, len(sequence_for_permutations)):
+            permutation_list.append(sequence_for_permutations[:item] + letter + sequence_for_permutations[item:])
+            # iterate over sequence_for_permutations, inserting item into each location
+        return get_permutations(permutation_list)
+
+
+        # for x in range(len(sequence)):
+        #     pass
+        # pop the first letter
+        # get permutations of what's left
+        # return sequence
+    # permutation abcd = a into permutation bcd = b into permutation cd = c into d
+
+
+
+example_input = 'abc'
+print('Input:', example_input)
+print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+print('Actual Output:', get_permutations(example_input))
 
 if __name__ == '__main__':
-#    #EXAMPLE
-#    example_input = 'abc'
-#    print('Input:', example_input)
-#    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
-#    print('Actual Output:', get_permutations(example_input))
-    
-#    # Put three example test cases here (for your sanity, limit your inputs
-#    to be three characters or fewer as you will have n! permutations for a 
-#    sequence of length n)
+    #    #EXAMPLE
+    #    example_input = 'abc'
+    #    print('Input:', example_input)
+    #    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+    #    print('Actual Output:', get_permutations(example_input))
 
-    pass #delete this line and replace with your code here
+    #    # Put three example test cases here (for your sanity, limit your inputs
+    #    to be three characters or fewer as you will have n! permutations for a
+    #    sequence of length n)
+
+    pass
 
